1c9a4f6de2e36cdfe2ab203ca84ddb3ec56a0571
Harsh Arora <harsh.aror@spektrasystems.com>
2023-03-28 12:54:41 +0530

    social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ import logo from "./images/logo.png";
 const Footer = () => {
   return (
     <>
-      <section class="w3l-footer-29-main">
-        <div class="footer-29 py-5">
-          <div class="container py-lg-4">
-            <div class="row footer-top-29">
-              <div class="col-lg-6 col-md-6 footer-list-29 footer-1 pr-lg-5">
-                <div class="footer-logo mb-4">
-                  <Link className="navbar-brand" to="/">
-                    <span className="fa">
+      <section className="w3l-footer-29-main">
+        <div className="footer-29 py-5">
+          <div className="container py-lg-4">
+            <div className="row footer-top-29">
+              <div className="col-lg-6 col-md-6 footer-list-29 footer-1 pr-lg-5">
+                <div className="footer-logo mb-4">
+                  <Link classNameName="navbar-brand" to="/">
+                    <span classNameName="fa">
                       <img
                         src={logo}
                         alt="weddinvi-logo"
@@ -29,9 +29,9 @@ const Footer = () => {
                   services for you.
                 </p>
               </div>
-              {/* <div class="col-lg-4 col-md-6 col-sm-5 col-5 footer-list-29 footer-2 mt-md-0 mt-5">
+              {/* <div className="col-lg-4 col-md-6 col-sm-5 col-5 footer-list-29 footer-2 mt-md-0 mt-5">
                 <ul>
-                  <h6 class="footer-title-29">Quick Links</h6>
+                  <h6 className="footer-title-29">Quick Links</h6>
                   <li>
                     <a href="about.html">About Us</a>
                   </li>
@@ -49,40 +49,89 @@ const Footer = () => {
                   </li>
                 </ul>
               </div> */}
-              <div class="col-lg-6 col-md-6 col-sm-7 col-7 footer-list-29 footer-3 mt-lg-0 mt-5">
-                <h6 class="footer-title-29">Head Office</h6>
-                <p class="mb-3">Y - 2089 Mangolpuri New delhi Delhi - 110083</p>
-                <p class="mb-2">
+              <div className="col-lg-6 col-md-6 col-sm-7 col-7 footer-list-29 footer-3 mt-lg-0 mt-5">
+                <h6 className="footer-title-29">Head Office</h6>
+                <p className="mb-3">
+                  Y - 2089 Mangolpuri New delhi Delhi - 110083
+                </p>
+                <p className="mb-2">
                   {" "}
-                  <span class="fa fa-phone"></span>{" "}
+                  <span className="fa fa-phone"></span>{" "}
                   <a href="tel:6387797327">+91 6387797327</a>
                 </p>
-                <p class="mb-2">
-                  <span class="fa fa-envelope-o"></span>{" "}
+                <p className="mb-2">
+                  <span className="fa fa-envelope-o"></span>{" "}
                   <a href="mailto:weddinvii@gmail.com">weddinvii@gmail.com</a>
                 </p>
               </div>
-              {/* <div class="col-lg-3 col-md-6 footer-list-29 footer-4 mt-lg-0 mt-5">
-                <h6 class="footer-title-29">Latest posts</h6>
-                <div class="post1">
-                  <a href="#url" class="post-title">
+              {/* <div className="col-lg-3 col-md-6 footer-list-29 footer-4 mt-lg-0 mt-5">
+                <h6 className="footer-title-29">Latest posts</h6>
+                <div className="post1">
+                  <a href="#url" className="post-title">
                     7 Banking Services That Can Save Retirees Money
                   </a>
-                  <p class="small">September 28, 2020</p>
+                  <p className="small">September 28, 2020</p>
                 </div>
-                <div class="post1 mt-4">
-                  <a href="#url" class="post-title">
+                <div className="post1 mt-4">
+                  <a href="#url" className="post-title">
                     Stocks Could Surge 10% between Now And 2020
                   </a>
-                  <p class="small">September 28, 2020</p>
+                  <p className="small">September 28, 2020</p>
                 </div>
               </div> */}
             </div>
           </div>
         </div>
-        <section class="w3l-copyright text-center">
-          <div class="container">
-            <p class="copy-footer-29">
+        <section className="w3l-copyright text-center">
+          <div className="container">
+            <div style={{ margin: "0 auto", justifyContent: "center" }}>
+              <ul
+                className="list-group list-group-horizontal"
+                style={{ background: "inherit" }}
+              >
+                <li
+                  className="list-group-item"
+                  style={{ background: "none", border: "none" }}
+                >
+                  <a href="https://youtube.com/@Weddinvi">
+                    <span className="fa fa-youtube-play"></span>
+                  </a>
+                </li>
+                <li
+                  className="list-group-item"
+                  style={{ background: "none", border: "none" }}
+                >
+                  <a href="https://instagram.com/weddinvi?igshid=ZDdkNTZiNTM=">
+                    <span className="fa fa-instagram"></span>
+                  </a>
+                </li>
+                <li
+                  className="list-group-item"
+                  style={{ background: "none", border: "none" }}
+                >
+                  <a href="https://twitter.com/weddinvii?t=9yWIIn6ri-g9T7TwkDOQjg&s=08">
+                    <span className="fa fa-twitter"></span>
+                  </a>
+                </li>
+                <li
+                  className="list-group-item"
+                  style={{ background: "none", border: "none" }}
+                >
+                  <a href="https://www.facebook.com/profile.php?id=100089804556666&mibextid=ZbWKwL">
+                    <span className="fa fa-facebook"></span>
+                  </a>
+                </li>
+                <li
+                  className="list-group-item"
+                  style={{ background: "none", border: "none" }}
+                >
+                  <a href="https://pin.it/3fgGEhO">
+                    <span className="fa fa-pinterest"></span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <p className="copy-footer-29">
               © 2023 Weddinvi Cards. All rights reserved.
             </p>
           </div>
